112728c0b990ff30577fdbaffbcfbb04272ed32c
Daulet <35165187+dauletalzhanov@users.noreply.github.com>
2023-10-27 14:29:55 +0100

    exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(word) {
+	array = word.split('')
+
+	output = []
+
+
+	while (array.length != 0) {
+		output.push(array.pop())
+	}
+
+	return output = output.join('')
 
 };
+
+reverseString('potato')
 
 // Do not edit below this line
 module.exports = reverseString;
