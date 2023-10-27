d33a58228ca8286184c72aea9690ca446417e113
Daulet <35165187+dauletalzhanov@users.noreply.github.com>
2023-10-27 17:00:11 +0100

    exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+	for (let i = 0; i<args.length; i++){
+
+		if (array.includes(args[i]) != true) {
+			continue
+		}
+		array.splice(array.indexOf(args[i]), 1)
+		console.log(array)
+	}
+
+	// array = array.join('').split('')
+
+	// console.log(array)
+
+	// for (let i = 0; i<array.length;i++) {
+	// 	array[i] = parseInt(array[i])
+
+	// }
+
+	//console.log(array)
+
+	return array
 
 };
+
+//console.log(removeFromArray([1, 2, 3], "1", 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
