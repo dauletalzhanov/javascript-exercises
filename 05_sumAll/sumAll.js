0ff6c3957ce2448b8ab9ecf4e04bc064de2b7fee
Daulet <35165187+dauletalzhanov@users.noreply.github.com>
2023-10-27 17:23:31 +0100

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+	if (!Number.isInteger(first) || !Number.isInteger(second))
+		return "ERROR"
+	else if (first<0 || second<0)
+		return "ERROR"
+
+	if (first > second) {
+		let temp = first
+		first = second
+		second = temp
+	}
+
+	let array = [first]
+	let counter = 0
+	let sum = first
+	
+	while (counter != second - 1) {
+		array.push(array[counter] + 1)
+		sum += array[array.length - 1]
+		counter++
+	}
+
+	//console.log(array)
+
+	return sum
 
 };
+
+
+console.log(sumAll(3, 1))
+
 
 // Do not edit below this line
 module.exports = sumAll;
