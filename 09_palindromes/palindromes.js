0f06f3f9d97ed8f51810b1fef6acd20c4a6f9b5d
Daulet <35165187+dauletalzhanov@users.noreply.github.com>
2023-11-16 23:38:31 +0000

    exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
+const palindromes = function (word) {
+	let w_o_r_d = word.toLowerCase()
+	w_o_r_d = w_o_r_d.split("")
+	w_o_r_d = w_o_r_d.filter((i) => ((48 <= i.charCodeAt(0) && i.charCodeAt(0) <= 57) || (97 <= i.charCodeAt(0) && i.charCodeAt(0) <= 122))) //(i.toLowerCase() != i.toUpperCase()))
+
+
+	//console.log(w_o_r_d)
+
+	let i = 0
+	while (i != w_o_r_d.length) {
+		if (w_o_r_d[i] != w_o_r_d[w_o_r_d.length - i - 1]) {
+			console.log(false)
+			return false
+		}
+
+		i++
+
+	}
+	console.log(true)
+	return true
 
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+palindromes("r3ace3car")
+palindromes("Animal loots foliated detail of stool lamina.")
+palindromes("A car, a man, a maraca.")
+palindromes("Racecar!")
+palindromes("racecar!")
+palindromes('ZZZZ car, a man, a maracaz.')
